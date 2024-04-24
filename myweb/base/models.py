6822f60db69c6ffdb7a9ad094c43c4c52fc54c52
12DoRoRo12 <n.tsitsqishvili@gmail.com>
2024-04-24 23:53:55 +0400

    CRUD

--- myweb/base/models.py
+++ myweb/base/models.py
@@ -16,6 +16,10 @@ class Room(models.Model):
     updated = models.DateTimeField(auto_now=True) #when updated
     created = models.DateTimeField(auto_now_add=True) #when created
 
+    #make new added room appier first in home page (- reverses)
+    class Meta:
+        ordering = ['-updated', '-created']
+
     def __str__(self):
         return self.name
 
--- myweb/base/views.py
+++ myweb/base/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # Create your views here.
 from .models import Room
-
+from .forms import RoomForm
 # rooms = [
 #     {'id': 1, "name": "Let's learn Python!"},
 #     {'id': 2, "name": "Let's learn Javascript!"},
@@ -36,3 +36,36 @@ def room(request, pk):
     room = Room.objects.get(id=int(pk))
     context = {"room": room}
     return render(request, "base/room.html", context)
+
+
+def create_room(request):
+    form = RoomForm()
+    if request.method == "POST":
+        # print(request.POST)
+        # print(request.POST.get('name'))
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == "POST":
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
